940692b9bfc5e628758208c396def0f5b7c1f69a
Ryan Spencer <rspencer@bitovi.com>
2022-07-28 16:31:16 -0400

    add type checking on incoming props

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React, { Requireable } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import PropTypes from 'prop-types';
+import PropTypes, { InferProps } from 'prop-types';
 import reactToWebComponent from 'react-to-webcomponent';
 import Checklist from './components/checklist/Checklist';
+import { isChecklistItemType } from './types/checklistItem';
 
 class WebComponentWrapper extends React.Component<{ items: string }, {}> {
   static propTypes: {
@@ -10,7 +11,20 @@ class WebComponentWrapper extends React.Component<{ items: string }, {}> {
   };
 
   render() {
-    const items = JSON.parse(this.props.items);
+    let items;
+    try {
+      items = this.props.items ? JSON.parse(this.props.items) : undefined;
+      if (!Array.isArray(items)) {
+        items = undefined;
+      } else if (items.length === 0) {
+        items = undefined;
+      } else if (items?.some(item => !isChecklistItemType(item))) {
+        items = undefined;
+      }
+    } catch {
+      items = undefined
+    }
+
     return <Checklist items={items} />;
   }
 }
--- src/types/checklistItem.ts
+++ src/types/checklistItem.ts
@@ -2,3 +2,11 @@ export type ChecklistItemType = {
   label: string;
   isChecked: boolean;
 };
+
+export const isChecklistItemType = (value: any): value is ChecklistItemType => {
+  return (
+    typeof value === "object" &&
+    typeof value.label === "string" &&
+    typeof value.isChecked === "boolean"
+  );
+};
